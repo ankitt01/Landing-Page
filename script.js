119eb6fd6b9baebcf276234673eac968cb18bbff
ankitt01 <ankitrawat0102@gmail.com>
2021-01-05 22:18:48 +0530

    modified

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ const nav = document.querySelector('.nav')
 const close = document.getElementById('close').addEventListener('click', () => {
     console.log('clicked')
     nav.style.transform = 'translateX(100%)'
+    nav.style.display = 'none'
 });
 
 const open = document.getElementById('open').addEventListener('click', () => {
     console.log('clicked')
-    nav.style.transform = 'translateX(0)'
     nav.style.display = 'block'
+    setTimeout(() => {
+        nav.style.transform = 'translateX(0)'
+
+    },250)
 });
