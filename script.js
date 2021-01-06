7beca6fac9dbb295ee9983f402e0a1660e783171
ankitt01 <ankitrawat0102@gmail.com>
2021-01-06 20:01:44 +0530

    fixed the header styling and js

--- script.js
+++ script.js
@@ -4,12 +4,10 @@ const nav = document.querySelector('.nav')
 const close = document.getElementById('close').addEventListener('click', () => {
     console.log('clicked')
     nav.style.transform = 'translateX(100%)'
-    nav.style.display = 'none'
 });
 
 const open = document.getElementById('open').addEventListener('click', () => {
     console.log('clicked')
-    nav.style.display = 'block'
     setTimeout(() => {
         nav.style.transform = 'translateX(0)'
 
